9aa2738b67cac927802418397f5a76e8243e0167
MMahmud24 <mahinmahmud2406@gmail.com>
2026-08-29 11:23:10 -0400

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+        heap = []
+
+        for interval in intervals:
+            heapq.heappush(heap, (interval.start, interval.end))
+
+        end = -1 
+        while heap:
+            popped = heapq.heappop(heap)
+            if popped[0] < end:
+                return False
+
+            end = popped[1]
+
+        return True
